ea4d9d2329b5044cff7aa4cd02c6ac445867004a
Karlton Shoemaker <karltonshoemaker@gmail.com>
2021-02-05 15:34:06 -0500

    Added Player class. Added test class as well.

---
+++ Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Choose_Your_Class
+{
+    public class Player
+    {
+        private string name;
+        private int number;
+        private string position;
+        public int fightStamina;
+        public int penaltyTime;
+
+        public string Name { get; }
+
+        public Player()
+        {
+
+        }
+
+
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace Choose_Your_Class
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("IT'S HOCKEY TIME IN AMERICA!!!!!\nChoose your options:");
+            Console.WriteLine("1. Shoot at the goal");
+            Console.WriteLine("2. Defend a shot");
+            Console.WriteLine("3. Start a fight");
+            Console.WriteLine("4. Serve your time in the penalty box");
         }
     }
 }
